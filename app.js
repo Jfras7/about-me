85871208100884160a3cb3461b76af017b25326f
Joshua Frasure <joshfrasure2@gmail.com>
2021-10-26 20:32:43 -0400

    added first question

---
+++ app.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let userName = prompt("What is your name?")
+alert('Welcome '+ userName)
+
+let sport = prompt("Do I watch the NFL?")
+if (sport.toLowerCase() == ('yes')){
+    alert('Correct!')
+    console.log('Correct!')
+} 
+else{
+    alert('WRONG!')
+    console.log('WRONG!')
+}
+
